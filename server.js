a7b291d6b045844400a4aeb7b49d5733f5581048
Benji Sidi <benjisidi@gmail.com>
2020-08-13 18:48:09 +0100

    Fix port variable

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const main = async () => {
     })
   }
 
-  const port = process.env.port || 5000
+  const port = process.env.PORT || 5000
 
   app.listen(port, () => console.log(`Server started on port ${port}`))
 
